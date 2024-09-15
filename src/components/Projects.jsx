5893f638f5a8bc12289b2d9313af2e5324b5ff69
ahmed popa <151566611+ahmedpopa1@users.noreply.github.com>
2024-09-15 13:23:34 +0300

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,105 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { Container, Row, Col, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import l1 from "../assets/img/l1.png";
+import l2 from "../assets/img/l2.png";
+import l3 from "../assets/img/l3.png";
+import l4 from "../assets/img/l4.png";
+import l5 from "../assets/img/l5.png";
+import l6 from "../assets/img/l6.png";
+import l7 from "../assets/img/l7.png";
+import l8 from "../assets/img/l8.png";
+import l9 from "../assets/img/l9.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Blogging Sphere",
+      description: "a website for shaing thoughts and discover diffirent ideas",
+      imgUrl: l1,
+    },
+    {
+      title: "Porsche cars",
+      description: "a website for booking Porsche cars and buying it",
+      imgUrl: l2,
+    },
+    {
+      title: "Fittness Gym",
+      description: "a Moblie app for gym info and personal training routines",
+      imgUrl: l3,
+    },
+    {
+      title: "Market Place",
+      description: " a Mobile app for buying clothes,ELectronics and more!",
+      imgUrl: l4,
+    },
+    {
+      title: "ToDoList",
+      description: "a mobile app for saving tasks",
+      imgUrl: l5,
+    },
+    {
+      title: "World News",
+      description: "a website for daily news all over the world",
+      imgUrl: l6,
+    },
+    {
+   
+      title: "Game Valley",
+      description: "a website for games",
+      imgUrl: l7,
+    },
+    {
+      title: "Movies World",
+      description: "a website for movies like IMDB:)",
+      imgUrl: l8,
+    },
+    {
+      title: "My Profile :)",
+      description: "a website for My Profile :)",
+      imgUrl: l9,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>My Projects</h2>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
