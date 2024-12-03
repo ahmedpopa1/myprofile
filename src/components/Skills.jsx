5637025d03fba980fb481d9a9bdb2882e1ee7342
ahmed popa <151566611+ahmedpopa1@users.noreply.github.com>
2024-12-03 02:23:02 +0200

    changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -40,11 +40,11 @@ export const Skills = () => {
               >
                 <div className="item">
                   <img src={meter1} alt="Image" />
-                  <h5>Web Development</h5>
+                  <h5>Mobile Development</h5>
                 </div>
                 <div className="item">
                   <img src={meter3} alt="Image" />
-                  <h5>Mobile Development</h5>
+                  <h5>Web Development</h5>
                 </div>
                 <div className="item">
                   <img src={meter2} alt="Image" />
