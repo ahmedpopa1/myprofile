92bbd85be14f95d5f61bc8e06830ede241e8708a
ahmed popa <151566611+ahmedpopa1@users.noreply.github.com>
2025-01-02 06:01:21 +0200

    changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,93 +27,105 @@ export const Projects = () => {
         "A website for sharing thoughts and discovering different ideas",
       imgUrl: l1,
       extraInfo:
-        " A website for sharing thoughts and discovering different ideas ,This platform allows users to create, share, and explore blogs on various topics.",
+        "A website for sharing thoughts and discovering different ideas. This platform allows users to create, share, and explore blogs on various topics. Built with React.js and MongoDB.",
     },
     {
       title: "Porsche Cars",
       description: "A website for booking Porsche cars and buying them",
       imgUrl: l2,
       extraInfo:
-        "Users can book their favorite Porsche models, customize them, and view pricing details.",
+        "Users can book their favorite Porsche models, customize them, and view pricing details. Built with React.js and MongoDB.",
     },
     {
       title: "Fitness Gym",
       description: "A mobile app for gym info and personal training routines",
       imgUrl: l3,
       extraInfo:
-        "mobile app for gym info and Helps users track their fitness routines, monitor progress, and find gyms nearby.",
+        "A mobile app for gym info. Helps users track their fitness routines, monitor progress, and find gyms nearby. Built with React Native and Firebase.",
     },
     {
       title: "Market Place",
       description: "A mobile app for buying clothes, electronics, and more!",
       imgUrl: l4,
       extraInfo:
-        "A one-stop marketplace where users can purchase and sell a variety of productsand for buying clothes, electronics, and more!.",
+        "A one-stop marketplace where users can purchase and sell a variety of products and buy clothes, electronics, and more. Built with React Native and Firebase.",
     },
     {
       title: "To-Do List",
       description: "A mobile app for saving tasks",
       imgUrl: l5,
       extraInfo:
-        "Helps users organize their tasks, set reminders, and increase productivity.",
+        "Helps users organize their tasks, set reminders, and increase productivity. Built with React Native and Firebase.",
     },
     {
       title: "World News",
       description: "A website for daily news from all over the world",
       imgUrl: l6,
       extraInfo:
-        "A website for daily news from all over the world and Provides the latest news, updates, and trending stories from around the globe.",
+        "Provides the latest news, updates, and trending stories from around the globe. Built with React.js and MongoDB.",
     },
     {
       title: "Game Valley",
       description: "A website for games",
       imgUrl: l7,
       extraInfo:
-        "An online platform for discovering new games, including reviews and ratings.",
+        "An online platform for discovering new games, including reviews and ratings. Built with React.js and MongoDB.",
     },
     {
       title: "Movies World",
       description: "A website for movies like IMDB:)",
       imgUrl: l8,
       extraInfo:
-        "Browse movies, read reviews, watch trailers, and find ratings of the latest films for movies like IMDB:).",
+        "Browse movies, read reviews, watch trailers, and find ratings of the latest films. Built with React.js and MongoDB.",
     },
     {
       title: "Calculator",
       description: "An app for logic operations",
       imgUrl: l10,
       extraInfo:
-        "A simple yet effective calculator for various mathematical and logical operations.",
+        "A simple yet effective calculator for various mathematical and logical operations. Built with React Native and Firebase.",
     },
     {
       title: "Stopwatch",
       description: "A timer application",
       imgUrl: l11,
       extraInfo:
-        "Keep track of time with a simple stopwatch, with options for multiple timers.",
+        "Keep track of time with a simple stopwatch, with options for multiple timers. Built with React Native and Firebase.",
     },
     {
       title: "Dashboard",
       description: "A dashboard template for React projects",
       imgUrl: l12,
       extraInfo:
-        "A customizable, responsive dashboard template built with React.",
+        "A customizable, responsive dashboard template built with React.js and MongoDB.",
     },
     {
       title: "My Portfolio",
       description: "Popa's profile",
       imgUrl: l9,
       extraInfo:
-        "This is a portfolio website showcasing my projects, skills, and achievements.",
+        "This is a portfolio website showcasing my projects, skills, and achievements. Built with React.js and MongoDB.",
     },
   ];
 
   const handleProjectClick = (project) => {
     setSelectedProject(project);
+    setTimeout(() => {
+      const element = document.getElementById("projects");
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 1);
   };
 
-  const handleBackClick = () => {
+  const handleBackClick = (projectId) => {
     setSelectedProject(null);
+    setTimeout(() => {
+      const element = document.getElementById(projectId);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 100);
   };
 
   return (
@@ -133,13 +145,22 @@ export const Projects = () => {
                   </h2>
                   {selectedProject ? (
                     <div>
-                      <button className="btn-back" onClick={handleBackClick}>
+                      <button
+                        className="btn-back"
+                        onClick={() =>
+                          handleBackClick(
+                            selectedProject.title.replace(/\s+/g, "-")
+                          )
+                        }
+                      >
                         Return to Projects
                       </button>
                       <ProjectCard {...selectedProject} />
                       <div className="extra-info">
                         <h3>More About The Project</h3>
-                        <p>{selectedProject.extraInfo}</p>
+                        <p className="extra-info">
+                          {selectedProject.extraInfo}
+                        </p>
                       </div>
                     </div>
                   ) : (
@@ -157,6 +178,7 @@ export const Projects = () => {
                             {projects.map((project, index) => (
                               <ProjectCard
                                 key={index}
+                                id={project.title.replace(/\s+/g, "-")} 
                                 {...project}
                                 onClick={() => handleProjectClick(project)}
                               />
