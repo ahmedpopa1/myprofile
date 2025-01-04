080884523b9b699bad37b4010ed9bac58ec4e82b
ahmed popa <151566611+ahmedpopa1@users.noreply.github.com>
2025-01-05 00:32:22 +0200

    lol

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
-import gmail from "../assets/img"
 import { HashLink } from "react-router-hash-link";
 import { BrowserRouter as Router } from "react-router-dom";
 
